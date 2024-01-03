e51a44766e0441d8b6d8f2c655e58ed703556fbd
Akans Udoka <Akansudoka7@gmail.com>
2024-01-03 14:50:25 +0100

    Updated tag controller

---
+++ BlogWebsite/Controllers/AdminTagController.cs
@@ -0,0 +1,23 @@
+﻿using BlogWebsite.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWebsite.Controllers
+{
+    public class AdminTagController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag(AddTagRequest addTagRequest)
+        {
+            var name = addTagRequest.Name;
+            var displayName = addTagRequest.DisplayName;
+            return View("Add");
+        }
+    }
+}
